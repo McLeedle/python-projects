e233975298eae749a2434a2cc1df77561905c70d
Mcleedle <mmojunior@hotmail.com>
2019-01-25 20:14:33 -0800

    fixing the spacing in comments so the editor shuts up

--- Example2 Strings/example2.py
+++ Example2 Strings/example2.py
@@ -1,11 +1,11 @@
 print "This is my second example for Python Projects using strings."
 
-#Ask for info
+# Ask for info
 name = "jeff"
 age = 39
 color = "fuscia"
 
-#playing around with strings
+# playing around with strings
 print name.lower()
 print age
 print color.upper()
@@ -14,6 +14,7 @@ print "Your name is: %s. Your age is %s. Your favorite color is %s." % (name.upp
 print "The first letter of your name is: " + name[0]
 print "There are %s. Letters in your name." % (len(name))
 
+# making characters variable to calculate how many characters they typed
 characters = name + str(age) + color
 
 print "You typed in %s Characters" % (len(characters))
