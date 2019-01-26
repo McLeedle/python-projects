bd9f1b15d27df0ed24b744edf85a2c0aa1105e61
Unkeptroadrash <mmojunior@hotmail.com>
2019-01-26 11:41:45 -0800

    This is an example project that took a while to understand however this is covering the use of functions.

---
+++ Example5 Functions/example5.py
@@ -0,0 +1,29 @@
+# this is our fifth python lesson example showing what we've learned with functions
+import math
+
+
+def sqroot():
+    """returns the square root of the user's number"""
+    number = raw_input("Enter a number: ")
+    if int(number):
+        print math.sqrt(int(number))
+    else:
+        print "You must enter a number!"
+
+
+print "Welcome to our example!"
+sqroot()
+
+print "What you just saw was outputted from a function in our program!"
+print "Let's try another!"
+
+
+def largest_number(a=raw_input("Enter a number: "), b=raw_input("Enter a second number: ")):
+    """returns the largest of the two numbers entered"""
+    if int(a) and int(b):
+        print "The largest number your entered was: %s" % max(int(a), int(b))
+    else:
+        print "Please enter a valid number! "
+
+
+largest_number()
